70e088a5523b544b44ab260b55a277e7c28e7122
sddanila <sddanila@yahoo.co.uk>
2019-02-07 21:40:57 +0000

    adding solution to the advanced sorting method

--- sorting.js
+++ sorting.js
@@ -1,6 +1,36 @@
-var array = [10, 2, 5, 1, 9];
+// var array = [10, 2, 5, 1, 9];
 
-array.sort(function(a, b){
-    return a - b;
-});
-console.log(array);
+// array.sort(function(a, b){
+//     return a - b;
+// });
+// console.log(array);
+
+
+var students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+
+console.log(students.sort(function(a, b){
+    var nameA = a.name.toLowerCase();
+    var nameB = b.name.toLowerCase();
+    var ageA = a.age;
+    var ageB = b.age;
+    if (nameA < nameB){
+        return -1;
+    }
+    if (nameA > nameB){
+        return 1;
+    }
+    if (nameA = nameB){
+        if (ageA < ageB){
+            return 1;
+        }
+        if (ageA > ageB){
+            return -1;
+        }
+        return 0;
+    }
+}));
